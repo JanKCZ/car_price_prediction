5476a87a531df57eba662fc32e9d31f411b1951c
Jan Kolník <jankolnik@Jans-MacBook-Pro.local>
2021-01-23 23:44:17 +0100

    added more info, css styling, tooltips

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify, render_template, request, url_for
 import joblib
 import predict_car_price_v2 as predict_file
 
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,16 +10,13 @@ def home():
 
 @app.route("/predict", methods = ["POST"])
 def predict():
-
 	int_features = [x for x in request.form.values()]
 	prediction = predict_file.make_test_prediction(int_features)
 	return render_template('car_price.html',pred=f'Odhadovaná cena vozidla je {prediction:n} Kč')
 
-	# return render_template('car_price.html',pred='Odhadovaná cena vozidla je: {:0f} Kč'.format(prediction))
-
 if __name__ == '__main__':
-    # app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
-    app.run()
+    app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
+    # app.run()
 
 
 
